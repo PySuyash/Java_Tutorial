14ce4f879f672d09a67458ab89428b8da6735cc0
Suyash <unknownup31@gmail.com>
2026-08-05 21:46:21 +0530

    passed String[] args as arguments in the function

--- tutorial01.java
+++ tutorial01.java
@@ -1,5 +1,5 @@
 public class tutorial01 {
-    public static void main() {
+    public static void main(String[] args) {
         System.out.println("Hello this is my nery first line of Java code......");
     }
 }
