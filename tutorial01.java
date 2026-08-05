88ae4c4e70e65ef7866a5d88e782bf3ad4541471
Suyash <unknownup31@gmail.com>
2026-08-05 22:02:14 +0530

    Added some extra output statement

--- tutorial01.java
+++ tutorial01.java
@@ -3,5 +3,8 @@
 public class tutorial01 {
     public static void main(String[] args) {
         System.out.println("Hello this is my nery first line of Java code......");
+        System.out.println(34);
+        System.out.println(5*4);
+        System.out.println(5+6);
     }
 }
