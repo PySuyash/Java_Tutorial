1022115511f50ea9dab47619301d633c66a0dedc
Suyash <unknownup31@gmail.com>
2026-08-05 21:47:24 +0530

    Added the comment

--- tutorial01.java
+++ tutorial01.java
@@ -1,3 +1,5 @@
+// This is the first line of java code. #Tutorial->01
+
 public class tutorial01 {
     public static void main(String[] args) {
         System.out.println("Hello this is my nery first line of Java code......");
